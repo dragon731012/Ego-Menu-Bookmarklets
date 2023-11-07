30c7570cee9949eeb293c2c1e6388ea0091c7d90
ego <77611094+yeahbread@users.noreply.github.com>
2023-11-07 15:25:22 -0800

    Update Menu.js

--- Menu.js
+++ Menu.js
@@ -1266,95 +1266,63 @@ javascript:(function(){
               document.title = originalTitle;
               isClassroomCovered = false;
       
-              // Reset the favicon
               const currentFavicon = document.querySelector('link[rel="icon"]');
               if (originalFavicon && currentFavicon) {
                   document.head.removeChild(currentFavicon);
                   document.head.appendChild(originalFavicon);
               }
           } else {
-              const content = `
-                  <div class="EgoWindowPopoutTitle">Panic Key Configuration</div>
-                  <p>Enter the key that should trigger the cover:</p>
-                  <input type="text" id="triggerKeyInput" class="EgoInput" placeholder="Enter key...">
-                  <p>Select the image to use:</p>
-                  <img id="disconnectImg" src="https://github.com/yeahbread/Ego-Menu-Bookmarklets/blob/main/disconnect.png?raw=true" width="20" height="20" style="cursor: pointer;">
-                  <img id="resetImg" src="https://github.com/yeahbread/Ego-Menu-Bookmarklets/blob/main/reset.png?raw=true" width="20" height="20" style="cursor: pointer;">
-                  <div id="buttonContainer">
-                      <button id="cancelBtn" class="EgoBtn">Cancel</button>
-                      <button id="saveBtn" class="EgoBtnPrimary">Save</button>
-                  </div>
-              `;
-              togglePopup(content);
+              const coverDiv = document.createElement('div');
+              coverDiv.id = 'classroom-cover';
+              coverDiv.style.width = '100vw';
+              coverDiv.style.height = '100vh';
+              coverDiv.style.position = 'fixed';
+              coverDiv.style.top = '0';
+              coverDiv.style.left = '0';
+              coverDiv.style.backgroundColor = '#fff';
+              coverDiv.style.zIndex = '9999999999999999999999999';
+              coverDiv.style.padding = '0';
+              coverDiv.style.margin = '0';
       
-              const triggerKeyInput = document.getElementById('triggerKeyInput');
-              const disconnectImg = document.getElementById('disconnectImg');
-              const resetImg = document.getElementById('resetImg');
-              const cancelBtn = document.getElementById('cancelBtn');
-              const saveBtn = document.getElementById('saveBtn');
+              const coverImage = document.createElement('img');
+              coverImage.src = 'https://raw.githubusercontent.com/yeahbread/Ego-Menu-Bookmarklets/main/disconnect.png';
+              coverImage.style.width = '100%';
+              coverImage.style.height = '100%';
+              coverImage.style.position = 'absolute';
+              coverImage.style.top = '0';
+              coverImage.style.left = '0';
+              coverImage.style.zIndex = '1';
       
-              disconnectImg.addEventListener('click', () => {
-                  disconnectImg.style.border = '2px solid #2196F3';
-                  resetImg.style.border = 'none';
-              });
+              coverDiv.appendChild(coverImage);
       
-              resetImg.addEventListener('click', () => {
-                  resetImg.style.border = '2px solid #2196F3';
-                  disconnectImg.style.border = 'none';
-              });
+              const currentFavicon = document.querySelector('link[rel="icon"]');
+              if (currentFavicon) {
+                  originalFavicon = currentFavicon.cloneNode(true);
+                  document.head.removeChild(currentFavicon);
+              }
       
-              cancelBtn.addEventListener('click', () => {
-                  togglePopup('', false);
-              });
+              const favicon = document.createElement('link');
+              favicon.rel = 'icon';
+              favicon.href = 'https://www.google.com/images/branding/googleg/1x/googleg_standard_color_128dp.png';
+              document.head.appendChild(favicon);
       
-              saveBtn.addEventListener('click', () => {
-                  const triggerKey = triggerKeyInput.value;
-                  const imgSrc = disconnectImg.style.border ? disconnectImg.src : resetImg.src;
-      
-                  // Validate the trigger key and image
-                  if (triggerKey.trim() === '') {
-                      triggerKeyInput.focus();
-                      return;
-                  } else if (imgSrc !== 'https://github.com/yeahbread/Ego-Menu-Bookmarklets/blob/main/disconnect.png?raw=true' &&
-                             imgSrc !== 'https://github.com/yeahbread/Ego-Menu-Bookmarklets/blob/main/reset.png?raw=true') {
-                      return;
-                  }
-      
-                  // Save the configuration and enable the panic key switch
-                  localStorage.setItem('panicKeyConfig', JSON.stringify({ triggerKey, imgSrc }));
-                  panicKeySwitch.checked = true;
-                  togglePopup('', false);
-              });
+              document.title = 'Error';
+              document.body.style.overflow = 'hidden';
+              document.body.appendChild(coverDiv);
+              isClassroomCovered = true;
           }
       }
       
       const panicKeySwitch = document.getElementById('panicKeySwitch');
       
       const handleKeyDown = (event) => {
-          const config = JSON.parse(localStorage.getItem('panicKeyConfig'));
-          if (event.key === config?.triggerKey) {
+          if (event.key === '`') {
               coverClassroom();
           }
       }
       
       panicKeySwitch.addEventListener('change', event => {
           if (event.target.checked) {
-              const config = JSON.parse(localStorage.getItem('panicKeyConfig'));
-              if (config) {
-                  const imgSrc = config.imgSrc;
-                  const currentFavicon = document.querySelector('link[rel="icon"]');
-                  if (currentFavicon) {
-                      // Store the original favicon
-                      originalFavicon = currentFavicon.cloneNode(true);
-                      document.head.removeChild(currentFavicon);
-                  }
-      
-                  const favicon = document.createElement('link');
-                  favicon.rel = 'icon';
-                  favicon.href = imgSrc;
-                  document.head.appendChild(favicon);
-              }
-      
               document.addEventListener('keydown', handleKeyDown);
           } else {
               document.removeEventListener('keydown', handleKeyDown);
