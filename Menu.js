f68ff97f551a40b06b41c9957fe47466a60bae42
ego <77611094+yeahbread@users.noreply.github.com>
2023-11-07 20:12:09 -0800

    Update Menu.js

--- Menu.js
+++ Menu.js
@@ -1258,6 +1258,8 @@ javascript:(function(){
       let isClassroomCovered = false;
       let originalTitle = document.title;
       let originalFavicon;
+      let coverImageSrc = 'https://github.com/yeahbread/Ego-Menu-Bookmarklets/blob/main/disconnect.png?raw=true';
+      let panicKey = '`';
       
       const coverClassroom = () => {
           if (isClassroomCovered) {
@@ -1266,6 +1268,7 @@ javascript:(function(){
               document.title = originalTitle;
               isClassroomCovered = false;
       
+              // Reset the favicon
               const currentFavicon = document.querySelector('link[rel="icon"]');
               if (originalFavicon && currentFavicon) {
                   document.head.removeChild(currentFavicon);
@@ -1285,7 +1288,7 @@ javascript:(function(){
               coverDiv.style.margin = '0';
       
               const coverImage = document.createElement('img');
-              coverImage.src = 'https://raw.githubusercontent.com/yeahbread/Ego-Menu-Bookmarklets/main/disconnect.png';
+              coverImage.src = coverImageSrc;
               coverImage.style.width = '100%';
               coverImage.style.height = '100%';
               coverImage.style.position = 'absolute';
@@ -1297,6 +1300,7 @@ javascript:(function(){
       
               const currentFavicon = document.querySelector('link[rel="icon"]');
               if (currentFavicon) {
+                  // Store the original favicon
                   originalFavicon = currentFavicon.cloneNode(true);
                   document.head.removeChild(currentFavicon);
               }
@@ -1310,16 +1314,55 @@ javascript:(function(){
               document.body.style.overflow = 'hidden';
               document.body.appendChild(coverDiv);
               isClassroomCovered = true;
+      
+              // Check if Ego Popup is open, close it if it is
+              if (document.getElementById('EgoPopout')) {
+                  closePopup();
+              }
+          }
+      }
+      
+      const settingsContent = `
+      <div class="EgoWindowPopoutTitle">Settings</div>
+        <div>
+          <label for="keyInput">Enter Panic Key:</label>
+          <input type="text" id="keyInput" class="EgoInput" value="${panicKey}">
+        </div>
+        <div>
+          <label for="imageSelect">Choose Cover Image:</label>
+          <select id="imageSelect" class="EgoSelect">
+              <option value="https://github.com/yeahbread/Ego-Menu-Bookmarklets/blob/main/disconnect.png?raw=true">Disconnect Icon</option>
+              <option value="https://github.com/yeahbread/Ego-Menu-Bookmarklets/blob/main/reset.png?raw=true">Reset Icon</option>
+          </select>
+        </div>
+        <button id="saveButton" class="EgoButton">Save</button>
+      `;
+      
+      const handleSaveSettings = (event) => {
+          panicKey = document.getElementById('keyInput').value;
+          const selectedImage = document.getElementById('imageSelect').value;
+          if (coverImageSrc !== selectedImage) {
+              coverImageSrc = selectedImage;
+              if (isClassroomCovered) {
+                  document.querySelector('#classroom-cover img').src = coverImageSrc;
+              }
+          }
+          closePopup();
+      }
+      
+      const settingsButton = document.getElementById('settingsButton');
+      settingsButton.addEventListener('click', event => {
+          togglePopup(settingsContent);
+          document.getElementById('saveButton').addEventListener('click', handleSaveSettings);
+      });
+      
+      const handleKeyDown = (event) => {
+          if (event.key === panicKey) {
+              coverClassroom();
           }
       }
       
       const panicKeySwitch = document.getElementById('panicKeySwitch');
-      
-      const handleKeyDown = (event) => {
-          if (event.key === '`') {
-              coverClassroom();
-          }
-      }
       
       panicKeySwitch.addEventListener('change', event => {
           if (event.target.checked) {
