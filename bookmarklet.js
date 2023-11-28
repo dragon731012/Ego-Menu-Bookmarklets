45b65f483d88361aae9b1ebb0c122bb1c27082cf
ego <77611094+yeahbread@users.noreply.github.com>
2023-11-27 18:17:29 -0800

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,6 @@
 javascript:(function() {
   var scriptElement = document.createElement('script');
-  var sourceUrl = 'https://raw.githubusercontent.com/yeahbread/Ego-Menu-Bookmarklets/main/Menu.js';
+  var sourceUrl = 'https://cdn.jsdelivr.net/gh/yeahbread/Ego-Menu-Bookmarklets/Menu.js';
   fetch(sourceUrl)
     .then(response => response.text())
     .then(sourceCode => {
