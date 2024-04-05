b8d552da4f738afb137e88c272545ccaa6ec6c59
yeahbread <77611094+yeahbread@users.noreply.github.com>
2024-04-04 17:53:58 -0700

    better error handling

--- bookmarklet.js
+++ bookmarklet.js
@@ -2,10 +2,18 @@ javascript:(function() {
   var scriptElement = document.createElement('script');
   var sourceUrl = 'https://cdn.jsdelivr.net/gh/yeahbread/Ego-Menu-Bookmarklets/Menu.js';
   fetch(sourceUrl)
-    .then(response => response.text())
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.text();
+    })
     .then(sourceCode => {
       scriptElement.text = sourceCode;
       document.body.appendChild(scriptElement);
     })
-    .catch(error => console.error('Error fetching script:', error));
+    .catch(error => {
+      alert('Error fetching script: ' + error.message);
+      console.error('Error fetching script:', error);
+    });
 })();
